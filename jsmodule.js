d86b2e997d02740733c017a844d2b50648dc30e0
Jeff Fang <sanlyfang@gmail.com>
2014-11-30 22:46:23 +1100

    Code format

--- jsmodule.js
+++ jsmodule.js
@@ -1,50 +1,50 @@
-(function($) {
-	"use strict";
+(function ($) {
+    "use strict";
 
-	var modules = {};
+    var modules = {};
 
-	var defineModule = function(name, prototype) {
-		var module = function(options) {
-			// Merge user options with defaults
-			this.settings = $.extend({}, this.defaults, options);
+    var defineModule = function (name, prototype) {
+        var module = function (options) {
+            // Merge user options with defaults
+            this.settings = $.extend({}, this.defaults, options);
 
-			// Cache container element if provided
-			if (this.settings.el) {
-				this.$el = $(this.settings.el);
-			}
+            // Cache container element if provided
+            if (this.settings.el) {
+                this.$el = $(this.settings.el);
+            }
 
-			// Register event handlers if provided, make sure "this" always refers to current object instead of DOM
-			for (var e in this.events) {
-				var parts = $.trim(e).match(/^(\S+)\s*(.*)$/);
-				var eventName = parts[1];
-				var selector = parts[2];
-				var handler = this.events[e];
-				
-				var target = this.$el || $(document);
-				if (selector) target = target.find(selector);
-				target.on(eventName, this[handler].bind(this));
-			}
+            // Register event handlers if provided, make sure "this" always refers to current object instead of DOM
+            for (var e in this.events) {
+                var parts = $.trim(e).match(/^(\S+)\s*(.*)$/);
+                var eventName = parts[1];
+                var selector = parts[2];
+                var handler = this.events[e];
 
-			// Initialize
-			if (typeof(this.init) == 'function') {
-				this.init();
-			}
-		};
-			
-		module.prototype = prototype;
-		modules[name] = module;
-		return module;
-	}
+                var target = this.$el || $(document);
+                if (selector) target = target.find(selector);
+                target.on(eventName, this[handler].bind(this));
+            }
 
-	var module = function() {
-		if (arguments.length == 1) {
-			return modules[arguments[0]];
-		} else if (arguments.length == 2) {
-			return defineModule(arguments[0], arguments[1]);
-		}
-	}
+            // Initialize
+            if (typeof(this.init) == 'function') {
+                this.init();
+            }
+        };
 
-	$.extend({
-		module : module
-	});
+        module.prototype = prototype;
+        modules[name] = module;
+        return module;
+    }
+
+    var module = function () {
+        if (arguments.length == 1) {
+            return modules[arguments[0]];
+        } else if (arguments.length == 2) {
+            return defineModule(arguments[0], arguments[1]);
+        }
+    }
+
+    $.extend({
+        module: module
+    });
 })(jQuery);
